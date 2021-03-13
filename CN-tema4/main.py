2ff4374030e975ab30a5a4456622ed980079c77e
Ana Potoroaca <53191102+Ana-poto@users.noreply.github.com>
2021-03-13 16:49:03 +0200

    tema 4 - init

---
+++ CN-tema4/main.py
@@ -0,0 +1,21 @@
+a1_filename="files/a1.txt"
+a2_filename="files/a2.txt"
+a3_filename="files/a3.txt"
+a4_filename="files/a4.txt"
+a5_filename="files/a5.txt"
+
+f1_filename="files/f1.txt"
+f2_filename="files/f2.txt"
+f3_filename="files/f3.txt"
+f4_filename="files/f4.txt"
+f5_filename="files/f5.txt"
+
+epsilon = 10 **(-13)
+# CERINTA 1
+#Sa se calculeze cu Gauss-Seidel solutia sistemului Ax=f
+#pt A =ai.txt si f=fi.txt
+# CERINTA 2
+#Sa se verifice soultia xG, cu norma  ||A*xG-f|| (se poate folosi si norma din biblioteca)
+
+#A matrice rara
+#un singur xG
