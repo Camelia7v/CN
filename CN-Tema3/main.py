a406f7435880a88f3037c30367d0a03e11cd87ad
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2021-03-13 16:30:57 +0200

    progrese inmultire

--- CN-Tema3/main.py
+++ CN-Tema3/main.py
@@ -1,10 +1,9 @@
 import copy
-
 import methods
 
 # memorare matricea b
-# with open("b.txt", 'r') as f:
-with open("bTestCase.txt", 'r') as f:
+with open("b.txt", 'r') as f:
+# with open("bTestCase.txt", 'r') as f:
     lines = f.readlines()
     n = int(lines[0])
     p = int(lines[1])
@@ -24,12 +23,12 @@ print("b: ", len(b), b, "\n")
 print("c: ", len(c), c, "\n")
 
 # memorare matricea a
-# with open("a.txt", 'r') as f:
-with open("aTestCase.txt", 'r') as f:
+with open("a.txt", 'r') as f:
+# with open("aTestCase.txt", 'r') as f:
     lines = f.readlines()
     n = int(lines[0])
     m = methods.create_empty_list_of_dicts(n)
-    for i in range(2, 14):  # 7108):
+    for i in range(2,  7108):  # 14):
         if int(lines[i].split(',')[2]) in m[int(lines[i].split(',')[1])]:
             aux = m[int(lines[i].split(',')[1])].get(int(lines[i].split(',')[2]))
             m[int(lines[i].split(',')[1])][int(lines[i].split(',')[2])] = aux + float(lines[i].split(',')[0])
@@ -39,8 +38,6 @@ with open("aTestCase.txt", 'r') as f:
 print("m: ", len(m), m, "\n")
 
 # adunarea matricelor A+B
-
-
 aplusb = copy.deepcopy(m)
 for i in range(0, len(aplusb)):
     for j in aplusb[i].keys():
@@ -72,7 +69,7 @@ for i in range(0, len(c)):
             aplusb[i + 1][i] = c[i]
             break
 
-print("A + B: ", "\n", aplusb, "\n")
+# print("A + B: ", "\n", aplusb, "\n")
 
 # sortare apusb
 sorted_aplusb = methods.sort_matrix(aplusb)
@@ -80,12 +77,12 @@ sorted_aplusb = methods.sort_matrix(aplusb)
 print("sorted A + B", "\n", sorted_aplusb, "\n")
 
 # memorare matricea aplusb
-# with open("aplusb.txt", 'r') as f:
-with open("aplusbTestCase.txt", 'r') as f:
+with open("aplusb.txt", 'r') as f:
+# with open("aplusbTestCase.txt", 'r') as f:
     lines = f.readlines()
     n = int(lines[0])
     aplusb_din_fisier = methods.create_empty_list_of_dicts(n)
-    for i in range(2, 18):  # 11143):
+    for i in range(2, 11143):  # 18):
         aplusb_din_fisier[int(lines[i].split(',')[1])][int(lines[i].split(',')[2])] = float(lines[i].split(',')[0])
 
 print("A + B din fisier: ", "\n", aplusb_din_fisier)
@@ -106,23 +103,60 @@ for i in range(0, len(m1)):
             m1[i][j] = b[i]
         if i - j == p:
             m1[i][j] = c[j]
-print(" A: ", "\n", m)
-print(" B: ", "\n", m1)
+# print("A: ", "\n", m)
+# print("B: ", "\n", m1)
+
+# for i in range(0, len(aorib)):
+#     for j in set(range(0, n)) - set(aorib[i].keys()):
+#         aorib[i][j] = 0.0
 
 for i in range(0, len(aorib)):
-    for j in m1[i].keys():
-        for k in range(0, len(m1[i])):
-            if k in aorib[i] and j in aorib[i] and j in m1[k]:
-                aorib[i][j] += aorib[i][k] * m1[k][j]
+    for j in aorib[i].keys():
+        suma = 0
+        for l in m1[j].keys():
+            prod = 0
+            if j != l:
+                continue
+            for k in range(0, len(m1)):
+                if j == l == k:
+                    prod = aorib[i][j] * m1[k][l]
+                # if j-l == q == k:
+                #     prod = aorib[i][j] * m1[k][l]
+                # if l-j == l == k:
+                #     prod = aorib[i][j] * m1[k][l]
+            suma += prod
+        aorib[i].update({j: suma})
+        # aorib[i][j] = suma
 
-#         if i==j:
-#             aorib[i][j]=aorib[i][j]*m1[i][j]
-#         if j-i==q:
-#             aorib[i][j]=aorib[i][j]*m1[i][i+1]
-#         if i-j==p:
-#             aorib[i][j]=aorib[i][j]*m1[i][i-1]
+
+# for i in range(0, len(m1)):
+#     for j in m1[i].keys():
+#         suma = 0
+#         for l in aorib[j].keys():
+#             prod = 0
+#             for k in range(0, len(aorib)):
+#                 if j == l == k:
+#                     prod = m1[i][j] * aorib[k][l]
+#             suma += prod
+#         if aorib[i][j] == 0.0:
+#             aorib[i].update({j: suma})
+
+
+
+# X = [[102.5, 0, 2.5, 0, 0],
+#      [3.5, 104.88, 1.05, 0, 0.33],
+#      [0, 0, 100, 0, 0],
+#      [0, 1.3, 0, 101.3, 0],
+#      [0.73, 0, 0, 1.5, 102.23]
+#      ]
+# Y = [[102.5, 2.5, 0, 0, 0],
+#      [3.5, 104.88, 1.05, 0, 0],
+#      [0, 1.3, 100, 0.33, 0],
+#      [0, 0, 0.73, 101.3, 0],
+#      [0, 0, 0, 1.5, 102.23]
+#      ]
 #
-#
+# result = [[0, 0, 0, 0, 0] for i in range(0, 5)]
 # # iterate through rows of X
 # for i in range(len(X)):
 #    # iterate through columns of Y
@@ -130,16 +164,20 @@ for i in range(0, len(aorib)):
 #        # iterate through rows of Y
 #        for k in range(len(Y)):
 #            result[i][j] += X[i][k] * Y[k][j]
+# print(result)
 
 
 print("A * B: ", "\n", aorib)
 
 # memorare matricea aorib
-with open("aoribTestCase.txt", 'r') as f:
+# with open("aoribTestCase.txt", 'r') as f:
+with open("aorib.txt", 'r') as f:
     lines = f.readlines()
     n = int(lines[0])
     aorib_din_fisier = methods.create_empty_list_of_dicts(n)
-    for i in range(2, 23):
+    for i in range(2, 21285):
         aorib_din_fisier[int(lines[i].split(',')[1])][int(lines[i].split(',')[2])] = float(lines[i].split(',')[0])
 
 print("A * B din fisier: ", "\n", aorib_din_fisier)
+
+methods.equal(aorib, aorib_din_fisier)
--- CN-Tema3/methods.py
+++ CN-Tema3/methods.py
@@ -17,9 +17,9 @@ def equal(A, B):
         if verifying_two_dictionaries(B[i], A[i]) is False:
             count += 1
     if count > 0:
-        print("\n", "Matricele nu sunt egale", "\n")
+        print("\n", "Matricele NU sunt egale!", "\n")
     else:
-        print("\n", "Matricele sunt egale", "\n")
+        print("\n", "Matricele sunt egale!", "\n")
 
 
 def sort_matrix(a):
@@ -28,32 +28,3 @@ def sort_matrix(a):
         d = dict(sorted(a[i].items(), key=lambda j: j[0]))
         sorted_aplusb.append(d)
     return sorted_aplusb
-
-
-# # cautare valori cu aceiasi indici
-# for linie in range(0, len(m)):
-#     nr1 = 0
-#     for tupla in range(0, len(m[linie]) - 1):
-#         coloana = m[linie][tupla][1]
-#         nr = 0
-#         for tupla_urm in range(tupla + 1, len(m[linie])):
-#             if m[linie][tupla_urm][1] == coloana:
-#                 nr += 1
-#         if nr > 0:
-#             nr1 += 1
-#     # print(nr1)
-#
-# # eliminare valori cu aceiasi indici
-# for linie in range(0, len(m)):
-#     for tupla in range(0, len(m[linie]) - 1):
-#         coloana = m[linie][tupla][1]
-#         for tupla_urm in range(tupla + 1, len(m[linie])):
-#             if tupla_urm == len(m[linie]):
-#                 break
-#             if m[linie][tupla_urm][1] == coloana:
-#                 y1 = list(m[linie][tupla])
-#                 y2 = list(m[linie][tupla_urm])
-#                 y1[0] += y2[0]
-#                 m[linie][tupla] = tuple(y1)
-#                 m[linie][tupla_urm] = tuple(y2)
-#                 m[linie].remove((m[linie][tupla_urm][0], m[linie][tupla_urm][1]))
