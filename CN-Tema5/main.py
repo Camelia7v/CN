f95b73c725d4ebe565ba55d39c80cea87729060f
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2021-03-28 23:03:44 +0300

    Tema 5 - almost done

--- CN-Tema5/main.py
+++ CN-Tema5/main.py
@@ -1,8 +1,11 @@
+import methods
+import numpy
+import copy
+
 """
 p >= n
 p - nr. linii
 n - nr. coloane
-epsilon - precizia calculelor
 """
 
 """
@@ -11,9 +14,63 @@ epsilon - precizia calculelor
 
 p = 3
 n = 3
+# A = [[4, 2, 8],
+#      [2, 10, 10],
+#      [8, 10, 21]]
+# A_init = copy.deepcopy(A)
+
+A = [[1, 0, 1],
+     [0, 1, 0],
+     [1, 0, 1]]
+A_init = copy.deepcopy(A)
+
+# A = [[0, 1, 0],
+#      [1, 0, 1],
+#      [0, 1, 0]]
+# A_init = copy.deepcopy(A)
+
+rezultat = methods.jacobi_method(A, n)
+A = rezultat[0]
+U = rezultat[1]
+U_T = numpy.transpose(U)
+inm = numpy.dot(U_T, numpy.array(A_init))
+A_final = numpy.dot(inm, U)
+
+print(A)
+
+lambdaa = methods.get_lambda(A, n)
+a = numpy.dot(A_init, U)
+b = numpy.dot(U, lambdaa)
+norma = numpy.linalg.norm(a - b)
+print("Norma:", norma, "\n")
+
+
+"""
+2. 
+"""
+
+n = 3
 A = [[4, 2, 8],
      [2, 10, 10],
      [8, 10, 21]]
-A_init = [[4, 2, 8],
-          [2, 10, 10],
-          [8, 10, 21]]
+A_factorizat = methods.factorizarea_choleski(A)
+print(A_factorizat, "\n")
+
+
+"""
+3. 
+"""
+
+A = [[4, 2, 8],
+     [2, 10, 10],
+     [8, 10, 21]]
+U, S, V = numpy.linalg.svd(A, full_matrices=True)
+print("Valorile singulare ale matricei, S:", S)
+print("Rangul matricei A:", numpy.linalg.matrix_rank(A))
+print("Numarul de conditionare al matricei:", numpy.linalg.cond(A))
+A_I = numpy.linalg.pinv(A)
+print("Pseudoinversa Moore-Penrose a matricei:\n", A_I)
+AT = numpy.transpose(A)
+A_J = numpy.dot(numpy.linalg.inv(numpy.dot(AT, A)), AT)
+print("Pseudoinversa in sensul celor mai mici patrate:\n", A_J)
+print(numpy.linalg.norm(A_I - A_J))
--- CN-Tema5/methods.py
+++ CN-Tema5/methods.py
@@ -1,4 +1,5 @@
 import math
+import numpy
 
 
 def matricea_unitate(n):
@@ -14,17 +15,32 @@ def calculeaza_p_q(A, n):
     maximum = 0
     p = 0
     q = 0
+    # se cauta doar in partea strict inferior triunghiulara a matricei
     for i in range(n):
         for j in range(n):
-            # se cauta doar in partea strict inferior triunghiulara a matricei
-            if i > j and abs(A[i][j]) >= maximum:
+            if i < j and abs(A[i][j]) >= maximum:
                 maximum = abs(A[i][j])
                 p = i
                 q = j
     return p, q
 
 
-def calculeaza_unghiul_teta(A, p, q):
+def calculeaza_t(A, p, q):
+    # t = calculeaza_t(A, p, q)
+    # c = 1 / math.sqrt(1 + t * t)
+    # s = t / math.sqrt(1 + t * t)
+    if A[p][q] != 0 and A[p][p] != A[q][q]:
+        alfa = (A[p][p] - A[q][q]) / (2 * A[p][q])
+        print(alfa)
+        if alfa >= 0:
+            return - alfa + math.sqrt(alfa * alfa + 1)
+        else:
+            return - alfa - math.sqrt(alfa * alfa + 1)
+    else:
+        return 0
+
+
+def calculeaza_teta(A, p, q):
     if A[p][p] == A[q][q]:
         if A[p][q] > 0:
             return math.pi / 4
@@ -32,6 +48,31 @@ def calculeaza_unghiul_teta(A, p, q):
             return - math.pi / 4
     else:
         return 1 / 2 * math.tan(2 * A[p][q] / (A[p][p] - A[q][q]))
+
+
+def este_matrice_diagonala(A):
+    for i in range(len(A)):
+        for j in range(len(A)):
+            if i != j and abs(A[i][j]) > 10 ** (-8):
+                return True
+    return False
+
+
+def calculeaza_R_pq(n, p, q, c, s):
+    R_pq = [[0] * n for i in range(n)]
+    for i in range(len(R_pq)):
+        for j in range(len(R_pq)):
+            if i == j != p and i == j != q:
+                R_pq[i][j] = 1
+            elif i == j == p or i == j == q:
+                R_pq[i][j] = c
+            elif i == p and j == q:
+                R_pq[i][j] = s
+            elif i == q and j == p:
+                R_pq[i][j] = -s
+            else:
+                R_pq[i][j] = 0
+    return R_pq
 
 
 def jacobi_method(A, n):
@@ -43,20 +84,59 @@ def jacobi_method(A, n):
     U = matricea_unitate(n)
     p = calculeaza_p_q(A, n)[0]
     q = calculeaza_p_q(A, n)[1]
-    teta = calculeaza_unghiul_teta(A, p, q)
+    teta = calculeaza_teta(A, p, q)
     c = math.cos(teta)
     s = math.sin(teta)
-    t = math.tan(teta)
-    while k <= k_max:  # si A diferita de matricea diagonala
+    iteratie_finala = 0
+    while k <= k_max and este_matrice_diagonala(A) is True:
+        R_pq = calculeaza_R_pq(n, p, q, c, s)
+        A = numpy.array(A)
+        R_pq = numpy.array(R_pq)
+        R_pq_T = numpy.transpose(R_pq)
+        inm = numpy.dot(R_pq, A)
+        A = numpy.dot(inm, R_pq_T)
+        U = numpy.dot(U, R_pq_T)
+        p = calculeaza_p_q(A, n)[0]
+        q = calculeaza_p_q(A, n)[1]
+        teta = calculeaza_teta(A, p, q)
+        c = math.cos(teta)
+        s = math.sin(teta)
+        iteratie_finala = k
+        k = k + 1
+    print("iteratie finala:", iteratie_finala)
+    return A, U
+
+
+def get_lambda(A, n):
+    lambdaa = [[0] * n for i in range(n)]
+    for i in range(n):
+        lambdaa[i][i] = A[i][i]
+    return lambdaa
+
+
+def factorizarea_choleski(A):
+    k = 0
+    k_max = 1000
+
+    L_numpy = numpy.linalg.cholesky(A)
+    LT_numpy = numpy.transpose(L_numpy)
+    A_0 = numpy.dot(L_numpy, LT_numpy)
+
+    # L_numpy = numpy.linalg.cholesky(A_0)
+    # LT_numpy = numpy.transpose(L_numpy)
+    A_1 = numpy.dot(LT_numpy, L_numpy)
+
+    while k <= k_max and numpy.linalg.norm(A_1 - A_0) > 10 ** (-8):
+        L_numpy = numpy.linalg.cholesky(A_1)
+        LT_numpy = numpy.transpose(L_numpy)
+        A_1 = numpy.dot(L_numpy, LT_numpy)
+
+        A_0 = numpy.dot(LT_numpy, L_numpy)
+
+        # L_numpy = numpy.linalg.cholesky(A_0)
+        # LT_numpy = numpy.transpose(L_numpy)
+        # A_1 = numpy.dot(L_numpy, LT_numpy)
 
         k = k + 1
-
-
-if __name__ == "__main__":
-    A = [[4, 2, 8],
-         [2, 10, 10],
-         [8, 10, 21]]
-    p = calculeaza_p_q(A, 3)[0]
-    q = calculeaza_p_q(A, 3)[1]
-    print(p, q)
-    print(calculeaza_unghiul_teta(A, p, q))
+    print("iteratie finala:", k)
+    return A_1
