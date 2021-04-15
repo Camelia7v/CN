7f0f6bcac2360a028b027fe0346f86955b8273c4
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2021-04-15 15:45:10 +0300

    Tema 6: cerinta 2 + bonus

--- CN-Tema6/main.py
+++ CN-Tema6/main.py
@@ -1,5 +1,7 @@
 import methods
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 """
@@ -37,10 +39,75 @@ while True:
         x_barat = random.uniform(x[0], x[n])
 print("x_barat:", x_barat)
 
+
+"""
+CERINTA 1
+"""
+
+print("\n CERINTA 1 \n")
+
+# Se vor folosi valori ale lui m mai mici ca 6.
 m = 3
 print("m:", m)
 
-print("aproximarea lui f(x_barat) =", methods.metoda_celor_mai_mici_patrate(x_barat, x, y, n, m))
+P_de_x_barat = methods.metoda_celor_mai_mici_patrate(x_barat, x, y, n, m)
+print("P(x_barat), aproximarea lui f(x_barat) =", P_de_x_barat)
 print("f(x_barat) =", f(x_barat))
+print("|P(x_barat) - f(x_barat)| =", abs(P_de_x_barat - f(x_barat)))
+
+suma = 0
+for i in range(n+1):
+    P_de_x = methods.metoda_celor_mai_mici_patrate(x[i], x, y, n, m)
+    suma += abs(P_de_x - y[i])
+print("Suma =", suma)
+
 
 # print(methods.horner_method([1, -12, 30], 0))
+
+
+"""
+CERINTA 2
+"""
+
+print("\n CERINTA 2 \n")
+
+f_derivat = lambda x: 2 * x - 12
+f_derivat_de_a = f_derivat(a)
+print("f'(a) =", f_derivat_de_a)
+
+S_de_x_barat = methods.spline_patratice(x_barat, f_derivat_de_a, x, y, n)
+print("S(x_barat), aproximarea lui f(x_barat) =", S_de_x_barat)
+print("f(x_barat) =", f(x_barat))
+print("|S(x_barat) - f(x_barat)| =", abs(S_de_x_barat - f(x_barat)))
+
+
+"""
+BONUS
+"""
+x1 = np.arange(a, b, 0.01)
+y1 = x1 ** 2 - 12 * x1 + 30
+y2 = list()
+for i in x1:
+    y2.append(methods.metoda_celor_mai_mici_patrate(i, x, y, n, m))
+y3 = list()
+for i in x1:
+    y3.append(methods.spline_patratice(i, f_derivat_de_a, x, y, n))
+
+# plotting the points
+plt.plot(x1, y1, label="f(x)", linewidth=7.0)
+# plt.plot(x1, y2, label="P_m(x)")
+plt.plot(x1, y3, label="S_f(x)", linewidth=2.0)
+
+# naming the axis
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+
+# giving a title
+plt.title('The three graphs')
+
+# show a legend on the plot
+plt.legend()
+# show the grid
+plt.grid(True)
+# show the plot
+plt.show()
--- CN-Tema6/methods.py
+++ CN-Tema6/methods.py
@@ -33,8 +33,22 @@ def metoda_celor_mai_mici_patrate(x_barat, x, y, n, m):
     a = numpy.linalg.solve(B, f)
     # print(numpy.allclose(numpy.dot(B, a), f))
     # a = numpy.array(a)
-    print("B:", B)
-    print("a:", a, a[0])
-    print("f:", f)
+
+    # print("B:", B)
+    # print("a:", a, a[0])
+    # print("f:", f)
     f_de_x_barat = horner_method(a, x_barat)
     return f_de_x_barat
+
+
+def spline_patratice(x_barat, f_derivat_de_a, x, y, n):
+    h = [0 for i in range(n+1)]
+    A = [0 for i in range(n+1)]
+    A[0] = f_derivat_de_a
+    for i in range(n):
+        h[i] = x[i+1] - x[i]
+        A[i+1] = -A[i] + (2 * (y[i+1] - y[i])) / h[i]
+    for i in range(n):
+        if x[i] < x_barat < x[i+1]:
+            S_de_x_barat = (A[i+1] - A[i]) / (2 * h[i]) * ((x_barat - x[i]) ** 2) + A[i] * (x_barat - x[i]) + y[i]
+            return S_de_x_barat
