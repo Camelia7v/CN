726692f1c892080c11d8bd14285c5f6a297dfa3a
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2021-04-28 16:38:29 +0300

    adaugat compararea solutiilor obtinute

--- CN-Tema8/main.py
+++ CN-Tema8/main.py
@@ -1,23 +1,36 @@
-import math
-import numpy
 import methods
+import random
+
 
 epsilon = 10 ** (-8)
 h = 10 ** (-5)
+# F = [1/3, -2, 2, 3]
+F = [1, -6, 13, -12, 4]
 
-F = [1/3, -2, 2, 3]
-# F = [1, -6, 13, -12, 4]
-F1 = numpy.polyder(F, 1)  # F'
 
-x_stelat = methods.dehghan_hajarian_method(F1, epsilon)
+i = 1
+x0 = random.uniform(-100, 100)
+x_stelat = methods.dehghan_hajarian_method(x0, F, i, h, epsilon)
 while x_stelat is None:
-    x_stelat = methods.dehghan_hajarian_method(F1, epsilon)
+    x0 = random.uniform(-100, 100)
+    x_stelat = methods.dehghan_hajarian_method(x0, F, i, h, epsilon)
 
 if methods.F_derivat_secund(F, x_stelat, h) > 0:
     print(methods.F_derivat_secund(F, x_stelat, h))
     print("Punctul critic x* =", x_stelat,
           "calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F.")
 else:
-    print(methods.F_derivat_secund(F, x_stelat, h))
+    print("Punctul critic x* =", x_stelat,
+          "calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!")
+print("\n")
+
+
+i = 2
+x_stelat = methods.dehghan_hajarian_method(x0, F, i, h, epsilon)
+
+if methods.F_derivat_secund(F, x_stelat, h) > 0:
+    print("Punctul critic x* =", x_stelat,
+          "calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F.")
+else:
     print("Punctul critic x* =", x_stelat,
           "calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!")
--- CN-Tema8/methods.py
+++ CN-Tema8/methods.py
@@ -1,6 +1,3 @@
-import random
-
-
 def horner_method(polinom, x):
     """
     Calculeaza valoarea polinomului in punctul x
@@ -11,44 +8,13 @@ def horner_method(polinom, x):
     return rezultat
 
 
-def dehghan_hajarian_method(g, epsilon):
-    """
-     Aproximarea unui punct critic al functiei F
-     Punct critic: radacina a functiei g(x) = F'(x)
-    """
-    k = 0
-    k_maxim = 1000
-    x0 = random.uniform(-100, 100)
-    x = x0
-
-    z = x + horner_method(g, x) ** 2 / (horner_method(g, x + horner_method(g, x)) - horner_method(g, x))
-    delta_x = (horner_method(g, x) * (horner_method(g, z) - horner_method(g, x))) / \
-              (horner_method(g, x + horner_method(g, x)) - horner_method(g, x))
-
-    while epsilon <= abs(delta_x) <= 10 ** 8 and k <= k_maxim:
-        if abs(horner_method(g, x + horner_method(g, x)) - horner_method(g, x)) <= epsilon:
-            return x
-        z = x + horner_method(g, x) ** 2 / (horner_method(g, x + horner_method(g, x)) - horner_method(g, x))
-        delta_x = (horner_method(g, x) * (horner_method(g, z) - horner_method(g, x))) / \
-                  (horner_method(g, x + horner_method(g, x)) - horner_method(g, x))
-        x = x - delta_x
-        k += 1
-
-    if abs(delta_x) < epsilon:
-        # convergenta
-        return x
-    else:
-        # divergenta
-        return
-
-
 def F_derivat_o_data(F, x, h, i):
     """
     Calculeaza valoarea functiei F' in punctul x
     """
     if i == 1:
         return (3 * horner_method(F, x) - 4 * horner_method(F, x - h) + horner_method(F, x - 2 * h)) / (2 * h)
-    elif i == 2:
+    if i == 2:
         return (- horner_method(F, x + 2 * h) + 8 * horner_method(F, x + h) - 8 * horner_method(F, x - h)
                 + horner_method(F, x - 2 * h)) / (12 * h)
 
@@ -59,3 +25,38 @@ def F_derivat_secund(F, x, h):
     """
     return (- horner_method(F, x + 2 * h) + 16 * horner_method(F, x + h) - 30 * horner_method(F, x)
             + 16 * horner_method(F, x - h) - horner_method(F, x - 2 * h)) / (12 * (h ** 2))
+
+
+def dehghan_hajarian_method(x0, g, i, h, epsilon):
+    """
+     Aproximarea unui punct critic al functiei F
+     Punct critic: radacina a functiei g(x) = F'(x)
+    """
+    k = 0
+    k_maxim = 1000
+
+    x = x0
+    z = x + F_derivat_o_data(g, x, h, i) ** 2 / \
+        (F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i))
+    delta_x = (F_derivat_o_data(g, x, h, i) * (F_derivat_o_data(g, z, h, i) - F_derivat_o_data(g, x, h, i))) / \
+              (F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i))
+
+    while epsilon <= abs(delta_x) <= 10 ** 8 and k <= k_maxim:
+        if abs(F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i)) <= epsilon:
+            # convergenta
+            print("Pentru x0 = %d, folosind G%d(x,h), obtinem un numar de %d iteratii." % (x0, i, k))
+            return x
+        z = x + F_derivat_o_data(g, x, h, i) ** 2 / \
+            (F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i))
+        delta_x = (F_derivat_o_data(g, x, h, i) * (F_derivat_o_data(g, z, h, i) - F_derivat_o_data(g, x, h, i))) / \
+                  (F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i))
+        x = x - delta_x
+        k += 1
+
+    if abs(delta_x) < epsilon:
+        # convergenta
+        print("Pentru x0 = %d, folosind G%d(x,h), obtinem un numar de %d iteratii." % (x0, i, k))
+        return x
+    else:
+        # divergenta
+        return
