c6419c510baaaf6a2f4188a3017847a0dfb77dde
Ana-poto <a_potoroaca@yahoo.com>
2021-05-05 18:04:57 +0300

    am adaugat flag pentru sin/cos

--- CN-Tema8/main.py
+++ CN-Tema8/main.py
@@ -1,36 +1,44 @@
+from numpy import NaN
+
 import methods
 import random
 
 
 epsilon = 10 ** (-8)
 h = 10 ** (-5)
-# F = [1/3, -2, 2, 3]
-F = [1, -6, 13, -12, 4]
+F = [1/3, -2, 2, 3]
+# F = [1,0,1]
+# F = [1, -6, 13, -12, 4]
+flag = 1 # no trigo
+# flag = False # sin function on F last index
+# flag = True # cos function on F last index
 
 
 i = 1
 x0 = random.uniform(-10, 10)
-x_stelat = methods.dehghan_hajarian_method(x0, F, i, h, epsilon)
-while x_stelat is None:
-    x0 = random.uniform(-10, 10)
-    x_stelat = methods.dehghan_hajarian_method(x0, F, i, h, epsilon)
+x_stelat1 = methods.dehghan_hajarian_method(x0, F, flag, i, h, epsilon)
 
-if methods.F_derivat_secund(F, x_stelat, h) > 0:
-    print(methods.F_derivat_secund(F, x_stelat, h))
-    print("Punctul critic x* =", x_stelat,
+while x_stelat1 is None:
+    x0 = random.uniform(-10, 10)
+    x_stelat1 = methods.dehghan_hajarian_method(x0, F, flag, i, h, epsilon)
+
+if methods.F_derivat_secund(F, flag, x_stelat1, h) > 0:
+    # print(methods.F_derivat_secund(F, flag, x_stelat1, h))
+    print("Punctul critic x* =", x_stelat1,
           "calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F.")
 else:
-    print("Punctul critic x* =", x_stelat,
+    print("Punctul critic x* =", x_stelat1,
           "calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!")
 print("\n")
 
 
 i = 2
-x_stelat = methods.dehghan_hajarian_method(x0, F, i, h, epsilon)
+x_stelat2 = methods.dehghan_hajarian_method(x0, F, flag, i, h, epsilon)
 
-if methods.F_derivat_secund(F, x_stelat, h) > 0:
-    print("Punctul critic x* =", x_stelat,
+if methods.F_derivat_secund(F, flag, x_stelat2, h) > 0:
+    # print(methods.F_derivat_secund(F, flag, x_stelat2, h))
+    print("Punctul critic x* =", x_stelat2,
           "calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F.")
 else:
-    print("Punctul critic x* =", x_stelat,
+    print("Punctul critic x* =", x_stelat2,
           "calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!")
--- CN-Tema8/methods.py
+++ CN-Tema8/methods.py
@@ -1,33 +1,47 @@
-def horner_method(polinom, x):
+def horner_method(polinom, flag, x):
     """
     Calculeaza valoarea polinomului in punctul x
     """
     rezultat = polinom[0]
-    for i in range(1, len(polinom)):
-        rezultat = rezultat * x + polinom[i]
+
+    if flag == False:
+        for i in range(1, len(polinom) - 1):
+            rezultat = rezultat * x + polinom[i]
+        rezultat = rezultat * x + polinom[len(polinom) - 2]
+    elif flag == True:
+        for i in range(1, len(polinom)-1):
+            rezultat = rezultat * x + polinom[i]
+        rezultat = rezultat * x - polinom[len(polinom)-2]
+    else:
+        for i in range(1, len(polinom)-1):
+            rezultat = rezultat * x + polinom[i]
+
     return rezultat
 
 
-def F_derivat_o_data(F, x, h, i):
+
+def F_derivat_o_data(F, x, flag, h, i):
     """
     Calculeaza valoarea functiei F' in punctul x
     """
     if i == 1:
-        return (3 * horner_method(F, x) - 4 * horner_method(F, x - h) + horner_method(F, x - 2 * h)) / (2 * h)
+        return (3 * horner_method(F, flag, x) - 4 * horner_method(F, flag, x - h) + horner_method(F, flag, x - 2 * h)) / (2 * h)
     if i == 2:
-        return (- horner_method(F, x + 2 * h) + 8 * horner_method(F, x + h) - 8 * horner_method(F, x - h)
-                + horner_method(F, x - 2 * h)) / (12 * h)
+        return (- horner_method(F, flag, x + 2 * h) + 8 * horner_method(F, flag, x + h) - 8 * horner_method(F, flag, x - h)
+                + horner_method(F, flag, x - 2 * h)) / (12 * h)
 
 
-def F_derivat_secund(F, x, h):
+def F_derivat_secund(F, flag, x, h):
     """
     Calculeaza valoarea functiei F" in punctul x
     """
-    return (- horner_method(F, x + 2 * h) + 16 * horner_method(F, x + h) - 30 * horner_method(F, x)
-            + 16 * horner_method(F, x - h) - horner_method(F, x - 2 * h)) / (12 * (h ** 2))
+    if flag != 1:
+        flag = not flag
+    return (- horner_method(F, flag, x + 2 * h) + 16 * horner_method(F, flag, x + h) - 30 * horner_method(F, flag, x)
+            + 16 * horner_method(F, flag, x - h) - horner_method(F, flag, x - 2 * h)) / (12 * (h ** 2))
 
 
-def dehghan_hajarian_method(x0, g, i, h, epsilon):
+def dehghan_hajarian_method(x0, g, flag,i, h, epsilon):
     """
      Aproximarea unui punct critic al functiei g
      Punct critic: radacina a functiei g(x) = F'(x)
@@ -36,20 +50,20 @@ def dehghan_hajarian_method(x0, g, i, h, epsilon):
     k_maxim = 1000
 
     x = x0
-    z = x + (F_derivat_o_data(g, x, h, i) ** 2) / \
-        (F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i))
-    delta_x = (F_derivat_o_data(g, x, h, i) * (F_derivat_o_data(g, z, h, i) - F_derivat_o_data(g, x, h, i))) / \
-              (F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i))
+    z = x + (F_derivat_o_data(g, x, flag, h, i) ** 2) / \
+        (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h, i))
+    delta_x = (F_derivat_o_data(g, x, flag, h, i) * (F_derivat_o_data(g, z, flag, h, i) - F_derivat_o_data(g, x, flag, h, i))) / \
+              (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h, i))
 
     while epsilon <= abs(delta_x) <= 10 ** 8 and k <= k_maxim:
-        if abs(F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i)) <= epsilon:
+        if abs(F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag,h, i)) <= epsilon:
             # convergenta
             print("Pentru x0 = %d, folosind G%d(x,h), obtinem un numar de %d iteratii." % (x0, i, k))
             return x
-        z = x + (F_derivat_o_data(g, x, h, i) ** 2) / \
-            (F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i))
-        delta_x = (F_derivat_o_data(g, x, h, i) * (F_derivat_o_data(g, z, h, i) - F_derivat_o_data(g, x, h, i))) / \
-                  (F_derivat_o_data(g, x + F_derivat_o_data(g, x, h, i), h, i) - F_derivat_o_data(g, x, h, i))
+        z = x + (F_derivat_o_data(g, x, flag, h, i) ** 2) / \
+            (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h, i))
+        delta_x = (F_derivat_o_data(g, x, flag, h, i) * (F_derivat_o_data(g, z, flag, h, i) - F_derivat_o_data(g, x, flag, h, i))) / \
+                  (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h, i))
         x = x - delta_x
         k += 1
 
