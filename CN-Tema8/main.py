747a1759df1849ca5ca522d555da3cce65c10a3c
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2021-05-14 19:21:06 +0300

    Tema 8 - DONE

--- CN-Tema8/main.py
+++ CN-Tema8/main.py
@@ -1,16 +1,16 @@
 from numpy import NaN
-
 import methods
 import random
-message1=""
-message2=""
+
+message1 = ""
+message2 = ""
 
 epsilon = 10 ** (-8)
 h = 10 ** (-5)
-F = [1/3, -2, 2, 3]
+F = [1 / 3, -2, 2, 3]
 # F = [1,0,1]
 # F = [1, -6, 13, -12, 4]
-flag = 1 # no trigo
+flag = 1  # no trigo
 # flag = False # sin function on F last index
 # flag = True # cos function on F last index
 
@@ -26,15 +26,17 @@ while x_stelat1 is None:
 
 if methods.F_derivat_secund(F, flag, x_stelat1, h) > 0:
     # print(methods.F_derivat_secund(F, flag, x_stelat1, h))
-    message1 = ("Punctul critic x* = %f calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F." % (x_stelat1))
+    message1 = ("Punctul critic x* = %f calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F." % (
+        x_stelat1))
     print("Punctul critic x* =", x_stelat1,
           "calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F.")
 else:
-    message1 = ("Punctul critic x* = %f calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!" % (x_stelat1))
+    message1 = (
+            "Punctul critic x* = %f calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!" % (
+        x_stelat1))
     print("Punctul critic x* =", x_stelat1,
           "calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!")
 print("\n")
-
 
 i2 = 2
 k2 = 0
@@ -45,12 +47,15 @@ while x_stelat2 is None:
 
 if methods.F_derivat_secund(F, flag, x_stelat2, h) > 0:
     # print(methods.F_derivat_secund(F, flag, x_stelat2, h))
-    message2 = ("Punctul critic x* = %d calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F." % (x_stelat2))
+    message2 = ("Punctul critic x* = %d calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F." % (
+        x_stelat2))
     print("Punctul critic x* =", x_stelat2,
           "calculat cu metoda Dehghan-Hajarian este punct de minim pentru functia F.")
 else:
-    message2 = ("Punctul critic x* = %d calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!" % (x_stelat2))
+    message2 = (
+            "Punctul critic x* = %d calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!" % (
+        x_stelat2))
     print("Punctul critic x* =", x_stelat2,
           "calculat cu metoda Dehghan-Hajarian NU este punct de minim pentru functia F!")
 
-methods.cerinta_interface(x0,i1,i2,k1,k2,x_stelat1,x_stelat2,message1,message2)
+methods.cerinta_interface(x0, i1, i2, k1, k2, x_stelat1, x_stelat2, message1, message2)
--- CN-Tema8/methods.py
+++ CN-Tema8/methods.py
@@ -1,25 +1,25 @@
 import PySimpleGUI as gui
 
+
 def horner_method(polinom, flag, x):
     """
     Calculeaza valoarea polinomului in punctul x
     """
     rezultat = polinom[0]
 
-    if flag == False:
+    if flag is False:
         for i in range(1, len(polinom) - 1):
             rezultat = rezultat * x + polinom[i]
         rezultat = rezultat * x + polinom[len(polinom) - 2]
-    elif flag == True:
-        for i in range(1, len(polinom)-1):
+    elif flag is True:
+        for i in range(1, len(polinom) - 1):
             rezultat = rezultat * x + polinom[i]
-        rezultat = rezultat * x - polinom[len(polinom)-2]
+        rezultat = rezultat * x - polinom[len(polinom) - 2]
     else:
-        for i in range(1, len(polinom)-1):
+        for i in range(1, len(polinom) - 1):
             rezultat = rezultat * x + polinom[i]
 
     return rezultat
-
 
 
 def F_derivat_o_data(F, x, flag, h, i):
@@ -27,9 +27,11 @@ def F_derivat_o_data(F, x, flag, h, i):
     Calculeaza valoarea functiei F' in punctul x
     """
     if i == 1:
-        return (3 * horner_method(F, flag, x) - 4 * horner_method(F, flag, x - h) + horner_method(F, flag, x - 2 * h)) / (2 * h)
+        return (3 * horner_method(F, flag, x) - 4 * horner_method(F, flag, x - h) + horner_method(F, flag,
+                                                                                                  x - 2 * h)) / (2 * h)
     if i == 2:
-        return (- horner_method(F, flag, x + 2 * h) + 8 * horner_method(F, flag, x + h) - 8 * horner_method(F, flag, x - h)
+        return (- horner_method(F, flag, x + 2 * h) + 8 * horner_method(F, flag, x + h) - 8 * horner_method(F, flag,
+                                                                                                            x - h)
                 + horner_method(F, flag, x - 2 * h)) / (12 * h)
 
 
@@ -43,7 +45,7 @@ def F_derivat_secund(F, flag, x, h):
             + 16 * horner_method(F, flag, x - h) - horner_method(F, flag, x - 2 * h)) / (12 * (h ** 2))
 
 
-def dehghan_hajarian_method(x0, g, flag,i, h, epsilon):
+def dehghan_hajarian_method(x0, g, flag, i, h, epsilon):
     """
      Aproximarea unui punct critic al functiei g
      Punct critic: radacina a functiei g(x) = F'(x)
@@ -54,36 +56,45 @@ def dehghan_hajarian_method(x0, g, flag,i, h, epsilon):
     x = x0
     z = x + (F_derivat_o_data(g, x, flag, h, i) ** 2) / \
         (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h, i))
-    delta_x = (F_derivat_o_data(g, x, flag, h, i) * (F_derivat_o_data(g, z, flag, h, i) - F_derivat_o_data(g, x, flag, h, i))) / \
-              (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h, i))
+    delta_x = (F_derivat_o_data(g, x, flag, h, i) * (
+            F_derivat_o_data(g, z, flag, h, i) - F_derivat_o_data(g, x, flag, h, i))) / \
+              (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h,
+                                                                                                          i))
 
     while epsilon <= abs(delta_x) <= 10 ** 8 and k <= k_maxim:
-        if abs(F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag,h, i)) <= epsilon:
+        if abs(F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h,
+                                                                                                          i)) <= epsilon:
             # convergenta
             print("Pentru x0 = %d, folosind G%d(x,h), obtinem un numar de %d iteratii." % (x0, i, k))
-            return x,k
+            return x, k
         z = x + (F_derivat_o_data(g, x, flag, h, i) ** 2) / \
-            (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h, i))
-        delta_x = (F_derivat_o_data(g, x, flag, h, i) * (F_derivat_o_data(g, z, flag, h, i) - F_derivat_o_data(g, x, flag, h, i))) / \
-                  (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h, i))
+            (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x, flag, h,
+                                                                                                        i))
+        delta_x = (F_derivat_o_data(g, x, flag, h, i) * (
+                F_derivat_o_data(g, z, flag, h, i) - F_derivat_o_data(g, x, flag, h, i))) / \
+                  (F_derivat_o_data(g, x + F_derivat_o_data(g, x, flag, h, i), flag, h, i) - F_derivat_o_data(g, x,
+                                                                                                              flag, h,
+                                                                                                              i))
         x = x - delta_x
         k += 1
 
     if abs(delta_x) < epsilon:
         # convergenta
         print("Pentru x0 = %d, folosind G%d(x,h), obtinem un numar de %d iteratii." % (x0, i, k))
-        return x,k
+        return x, k
     else:
         # divergenta
         return
 
-def cerinta_interface(x0=0.0, i1=0, i2=0, k1=0, k2=0,x_stelat1=0.0,x_stelat2=0.0, message1=None, message2=None):
+
+def cerinta_interface(x0=0.0, i1=0, i2=0, k1=0, k2=0, x_stelat1=0.0, x_stelat2=0.0, message1=None, message2=None):
     gui.theme('DarkPurple6')
-    layout = [[gui.Text("\n" + ("Pentru x0 = %d, folosind G%d(x,h),\n obtinem un numar de %d iteratii." % (x0, i1, k1)), justification='center')],
-              [gui.Text(message1+"\n", justification='center')],
+    layout = [[gui.Text("\n" + ("Pentru x0 = %d, folosind G%d(x,h),\n obtinem un numar de %d iteratii." % (x0, i1, k1)),
+                        justification='center')],
+              [gui.Text(message1 + "\n", justification='center')],
               [gui.Text(("Pentru x0 = %d, folosind G%d(x,h),\n obtinem un numar de %d iteratii." % (x0, i2, k2)),
                         justification='center')],
-              [gui.Text(message2+"\n", justification='center')],
+              [gui.Text(message2 + "\n", justification='center')],
               [gui.Button("Close")]]
 
     # Create the window
